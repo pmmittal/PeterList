838e7eef8b392764a05f9e6ec0951facbb3cf83a
Parth <pmmittal@uci.edu>
2017-12-06 11:50:47 -0800

    PterList Sample Data + Modifications in Dataverse

--- generateTestData.py
+++ generateTestData.py
@@ -6,21 +6,20 @@ import random
 import time
 import math
 
-jobCategory_list = ['AccountingAdmin', 'ClericalArt', 'DesignAutomotiveBankingBiotechBusiness', \
- 'ManagementConstructionConsultantCustomer ServiceEducationEngineeringFilm', \
- 'VideoFinanceFood', 'BeverageGeneral LaborGovernmentHospitalityHuman ResourcesInformation TechnologyLegalManufacturingMarketing', \
- 'PRMedia', 'JournalismMedical', 'HealthNonprofitOtherReal EstateRetailSalesScienceSoftware', 'TechnologyTransportation']
+jobIndustry_list = ['Accounting','Art', 'Automotive','Banking', \
+ 'Management','Consultant','Customer Service','Education','Engineering', \
+ 'Video','Finance', 'Government','Hospitality','Human Resources','Information Technology','Legal','Manufacturing','Marketing', \
+ 'Journalism','Medical', 'Sales','Science','Software Technology','Transportation']
 
-eventCategory_list = ['Arts', 'MusicCamps, Trips', 'RetreatsCareerCharity', 'CausesClass, Training', \
-                      'WorkshopsConcerts', 'PerformanceConferences', 'SeminarsFestivals', 'FairsFood', \
-                      'DrinkGames', 'CompetitionsHobbies', 'Special InterestNetworkingOtherParties', \
-                      'Social GatheringsReligion', 'SpiritualityScience', 'TechnologySports', 'FitnessTravel', \
+eventCategory_list = ['Arts', 'Music','Camps Trip', 'Retreats','Career','Charity', 'Training', \
+                      'Workshops','Concerts','Festivals', 'Fairs','Food', \
+                      'Religion', 'Science', 'Technology','Sports', 'Fitness','Travel', \
                       'Outdoor']
 
-itemCategory_list = ['BicyclesBooksCars', \
-                     'MotorcyclesCell phonesClothing', 'AccessoriesComputersElectronicsFurnitureHealth', \
-                     'BeautyHousehold ItemsMusical InstrumentsOtherPhoto/VideoSkateboards', \
-                     'ScootersSporting GoodsTicketsToolsToys', 'GamesVideo Games']
+itemCategory_list = ['Bicycles','Books','Cars', \
+                     'Clothing', 'Electronics','Furniture','Health', \
+                     'Beauty','Household', 'Musical Instruments','Other', \
+                     'Toys']
 
 lostFoundCategory_list = ['Item Lost','Item Found']
 
@@ -53,9 +52,8 @@ def strTimeProp(start, end, format, prop):
 
     return time.strftime(format, time.localtime(ptime))
 
-
 def randomDate(start, end, prop):
-    return strTimeProp(start, end, 'datetime("%Y-%m-%dT%H:%M:%S")', prop)
+    return strTimeProp(start, end, "%Y-%m-%dT%H:%M:%S", prop)
 
 def get_random_domain(domains):
     return random.choice(domains)
@@ -89,34 +87,35 @@ random_street = ["Berkeley","Columbia","Oxford","Exter","Amherst Aisle","Harvard
 random_buildingNumber = [271,279,226,228,446,15121,65]
 city = ["Irvine","Newport Beach","Laguna Beach","Venice","Santa Monica"]
 
-jobTitle = ["Intern","Software Engineer","Business Analyst","Consultant"]
+jobTitle = ["Data Scientist","Software Engineer","Business Analyst","Consultant"]
 
-company_name = ["WellsFargo","Google","Chase","Yahoo","Facebook","Amazon","Verizon","Tesla"]
+company_name = ["Wells Fargo","Google","Chase","Yahoo","Facebook","Amazon","Verizon","Tesla","Apple"]
 true_false = ["true","false"]
-startDate = 'datetime("2017-05-20T00:00:00")'
-endDate = 'datetime("2018-03-16T23:59:59")'
+
+
+startDate = "2016-05-20T00:00:00"
+endDate = "2017-11-28T23:59:59"
 
 text_file = open("ownersampledata.adm","w")
 text_file2 = open("sampledata.adm","w")
 
 for i in range(1,101):
 
+    userID_postID_list = []
+
     userID = ID();
-    userPhone = phn();
-    useremailID = generate_random_emails(10,7)[1];
-    userPassword = password_generator()
-
-    randomemailID = generate_random_automated_emails(10,7);
     
-    user_string = '{{"userID": "{0}","contactInfo":{{"phoneNumber": "{1}","email": "{2}"}},"userPassword": "{3}"}}'
-    text_file.write(user_string.format(userID,userPhone,useremailID,userPassword))
-    text_file.write("\n")
+    
+    randomemailID = generate_random_automated_emails(10,7);
 
-    job_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"dateTime": {5},\
+
+#Job string done#
+    job_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"createdOn": "{5}",\
 "description": "{6}", "amount": {7}, "userID": "{8}", "automatedEmailID": "{9}"}}, "postingCategory": "Job",\
-"fullTime": {10}, "jobCategory": "{11}", "organizationName":"{12}", "jobTitle": "{13}", "jobRequirement": "{14}"}}'
+"jobType": "{10}", "jobIndustry": "{11}", "jobOrganizationName":"{12}", "jobTitle": "{13}", "jobRequirement": "{14}"}}'
 
     postID = ID()
+    userID_postID_list.append(postID)
     streetName = random.choice(random_street)
     buildingNumber = random.choice(random_buildingNumber)
     city = random.choice(city)
@@ -127,22 +126,26 @@ for i in range(1,101):
     userID = userID
     auto_email = random.choice(randomemailID)
 
-    value = random.choice(true_false)
-    jobCategory = random.choice(jobCategory_list)
+    jobType = random.choice(["Full Time","Part Time","Internship"])
+    #value = random.choice(true_false)
+    jobIndustry = random.choice(jobIndustry_list)
     organizationName = random.choice(company_name)
     jobTitle = random.choice(jobTitle)
-    jobRequirement = "Should have a Bachelor and Masters Degree with a 2 year work experience"
+    jobRequirement = "Should have a Bachelor and Masters Degree with a 2 years work experience"
 
-    text_file2.write(job_string.format(postID,streetName,buildingNumber,city,state,randomDateTime,description,amount,userID,auto_email,value,jobCategory,\
+    text_file2.write(job_string.format(postID,streetName,buildingNumber,city,state,randomDateTime,description,amount,userID,auto_email,jobType,jobIndustry,\
                                        organizationName,jobTitle,jobRequirement))
     text_file2.write("\n")
-    
 
-    event_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"dateTime": {5},\
-"description": "{6}", "amount": {7}, "userID": "{8}", "automatedEmailID": "{9}", "photos":"{15}"}}, "postingCategory": "Event",\
-"startDate": {10},"endDate": {11},"eventCategory": "{12}", "organizationName": "{13}", "eventTitle": "{14}"}}'
+
+    
+    #event string done**
+    event_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"createdOn": "{5}",\
+"description": "{6}", "amount": {7}, "userID": "{8}", "automatedEmailID": "{9}", "photos": ["{15}"]}}, "postingCategory": "Event",\
+"eventStartOn": "{10}","eventEndOn": "{11}","eventCategory": "{12}", "eventOrganizationName": "{13}", "eventTitle": "{14}"}}'
 
     postID = ID()
+    userID_postID_list.append(postID)
     streetName = random.choice(random_street)
     buildingNumber = random.choice(random_buildingNumber)
     city = random.choice(["Irvine","Newport Beach","Laguna Beach","Venice","Santa Monica"])
@@ -153,8 +156,8 @@ for i in range(1,101):
     userID = userID
     auto_email = random.choice(randomemailID)
 
-    startDateEvent = 'datetime("2017-06-01T00:00:00")'
-    endDateEvent = 'datetime("2017-06-29T23:59:59")'
+    startDateEvent = "2017-06-01T00:00:00"
+    endDateEvent = "2017-06-29T23:59:59"
     randomStartDate = randomDate(startDateEvent, endDateEvent, random.random())
     randomEndDate = randomDate(randomStartDate, endDateEvent, random.random())
     eventCategory = random.choice(eventCategory_list)
@@ -166,12 +169,15 @@ for i in range(1,101):
                                        randomEndDate,eventCategory,organizationName,eventTitle,eventPhoto))
     text_file2.write("\n")
 
-    itemSale_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"dateTime": {5},\
+    #Item_sale string done
+
+    itemSale_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"createdOn": "{5}",\
 "description": "{6}", "amount": {7}, "userID": "{8}", "automatedEmailID": "{9}"}}, "postingCategory": "Item Sale",\
 "itemName": "{10}","itemCategory": "{11}", "condition": "{12}"}}'
 
     
     postID = ID()
+    userID_postID_list.append(postID)
     streetName = random.choice(random_street)
     buildingNumber = random.choice(random_buildingNumber)
     city = random.choice(["Irvine","Newport Beach","Laguna Beach","Venice","Santa Monica"])
@@ -191,36 +197,39 @@ for i in range(1,101):
     text_file2.write("\n")
 
 
-    lostFound_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"dateTime": {5},\
-"description": "{6}", "amount": {7}, "userID": "{8}", "automatedEmailID": "{9}"}}, "postingCategory": "LostFound",\
-"itemFoundDate": {10}, "itemName": "{11}", "lostOrFound" : "{12}"}}'
+#     lostFound_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"dateTime": {5},\
+# "description": "{6}", "amount": {7}, "userID": "{8}", "automatedEmailID": "{9}"}}, "postingCategory": "LostFound",\
+# "itemFoundDate": {10}, "itemName": "{11}", "lostOrFound" : "{12}"}}'
 
+#     postID = ID()
+#     streetName = random.choice(random_street)
+#     buildingNumber = random.choice(random_buildingNumber)
+#     city = random.choice(["Irvine","Newport Beach","Laguna Beach","Venice","Santa Monica"])
+#     state = "CA"
+#     randomDateTime = randomDate(startDate, endDate, random.random())
+#     description = "Item on sale..."
+#     amount = 0
+#     userID = userID
+#     auto_email = random.choice(randomemailID)
+
+#     itemFoundDate = randomDate(startDate, randomDateTime, random.random())
+#     itemName = "Found Item Title"
+#     lostFound = random.choice(lostFoundCategory_list)
+
+#     text_file2.write(lostFound_string.format(postID,streetName,buildingNumber,city,state,randomDateTime,description,amount,userID,auto_email,itemFoundDate,\
+#                                             itemName,lostFound))
+#     text_file2.write("\n")
+
+
+
+    housing_sale_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"createdOn": "{5}",\
+"description": "{6}", "amount": {7}, "userID": "{8}", "automatedEmailID": "{9}", "photos":["{18}"]}}, "postingCategory": "Housing Sale",\
+"bedroomNumber":{10}, "bathroomNumber": {11},"homeType": "{12}","size": {13}, "dateAvailable": "{14}","furnished": {15}, "parkingNumber": {16},\
+"localityName" : "{17}"}}' 
+    
+    
     postID = ID()
-    streetName = random.choice(random_street)
-    buildingNumber = random.choice(random_buildingNumber)
-    city = random.choice(["Irvine","Newport Beach","Laguna Beach","Venice","Santa Monica"])
-    state = "CA"
-    randomDateTime = randomDate(startDate, endDate, random.random())
-    description = "Item on sale..."
-    amount = 0
-    userID = userID
-    auto_email = random.choice(randomemailID)
-
-    itemFoundDate = randomDate(startDate, randomDateTime, random.random())
-    itemName = "Found Item Title"
-    lostFound = random.choice(lostFoundCategory_list)
-
-    text_file2.write(lostFound_string.format(postID,streetName,buildingNumber,city,state,randomDateTime,description,amount,userID,auto_email,itemFoundDate,\
-                                            itemName,lostFound))
-    text_file2.write("\n")
-
-
-    housing_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"dateTime": {5},\
-"description": "{6}", "amount": {7}, "userID": "{8}", "automatedEmailID": "{9}", "photos":"{21}"}}, "postingCategory": "Housing",\
-"bedroomNumber":{10}, "petAllowed":{11},"housingCategory": "{12}","size": {13}, "startDate": {14},"endDate": {15}, "occupants": {16},\
-"bathroomType": {17}, "roommates": {18}, "hasParking": {19}, "localityName" : "{20}"}}'
-
-    postID = ID()
+    userID_postID_list.append(postID)
     streetName = random.choice(random_street)
     buildingNumber = random.choice(random_buildingNumber)
     city = random.choice(["Irvine","Newport Beach","Laguna Beach","Venice","Santa Monica"])
@@ -231,33 +240,88 @@ for i in range(1,101):
     userID = userID
     auto_email = random.choice(randomemailID)
 
-    bedroom = randint(1,5)
-    pet = random.choice(true_false)
-    housingCategory = "Rent/Lease"
+    bedroom = randint(1,7)
+    bathroom = randint(1,7)
+    homeType = random.choice(['Condo','House','Penthouse'])
+    #pet = random.choice(true_false)
+    #housingCategory = "Rent/Lease"
     size = randint(900,1200)
-    startDateEvent = 'datetime("2017-06-01T00:00:00")'
-    endDateEvent = 'datetime("2017-06-29T23:59:59")'
-    randomStartDate = randomDate(startDateEvent, endDateEvent, random.random())
-    randomEndDate = randomDate(randomStartDate, endDateEvent, random.random())
-    occupants = randint(2,7)
-    bathroom = random.choice(true_false)
-    roomates = randint(1,3)
-    hasParking = random.choice(true_false)
+    startDateEvent = "2017-06-01T00:00:00"
+    endDateEvent = "2017-06-29T23:59:59"
+    dateAvailable = randomDate(startDateEvent, endDateEvent, random.random())
+    furnished = random.choice(true_false)
+    parkingNumber = random.choice([1,2])
     locality = random.choice(["Harvard","Oxford","Columbia","Berkeley","Mesa"])
     housingPhoto = random.choice(housing_images)
+
+
+
     
 
-    text_file2.write(housing_string.format(postID,streetName,buildingNumber,city,state,randomDateTime,description,amount,userID,auto_email,bedroom,pet,\
-                                       housingCategory,size,randomStartDate,randomEndDate,occupants,bathroom,roomates,hasParking,locality,housingPhoto))
+    text_file2.write(housing_sale_string.format(postID,streetName,buildingNumber,city,state,randomDateTime,description,amount,userID,auto_email,bedroom,bathroom,\
+                                       homeType,size,dateAvailable,furnished,parkingNumber,locality,housingPhoto))
     text_file2.write("\n")
 
 
 
 
+    housing_lease_string = '{{"postID": "{0}", "postInfo":{{"location":{{"streetName": "{1}","buildingNumber": {2}, "city": "{3}","state": "{4}","country": "United States of America","zip": 92612}},"createdOn": "{5}",\
+"description": "{6}", "amount": {7}, "userID": "{8}", "automatedEmailID": "{9}", "photos":["{22}"]}}, "postingCategory": "Housing Lease",\
+"bedroomNumber": {10}, "bathroomNumber": {11},"homeType": "{12}","size": {13}, "dateAvailable": "{14}","furnished": {15}, "hasParking": {16},\
+"localityName" : "{17}", "petAllowed": {18}, "endDate": "{19}", "occupants": {20}, "roommates": {21}}}'
 
 
+    postID = ID()
+    userID_postID_list.append(postID)
+    streetName = random.choice(random_street)
+    buildingNumber = random.choice(random_buildingNumber)
+    city = random.choice(["Irvine","Newport Beach","Laguna Beach","Venice","Santa Monica"])
+    state = "CA"
+    randomDateTime = randomDate(startDate, endDate, random.random())
+    description = "Looking for someone..."
+    amount = amount1(400,1400)
+    userID = userID
+    auto_email = random.choice(randomemailID)
 
+    bedroom = randint(1,7)
+    bathroom = randint(1,7)
+    homeType = random.choice(['Condo','House','Penthouse'])
+    #pet = random.choice(true_false)
+    #housingCategory = "Rent/Lease"
+    size = randint(900,1200)
+    startDateEvent = "2017-06-01T00:00:00"
+    endDateEvent = "2017-06-29T23:59:59"
+    dateAvailable = randomDate(startDateEvent, endDateEvent, random.random())
+    furnished = random.choice(true_false)
+    hasParking = random.choice(true_false)
+    locality = random.choice(["Harvard","Oxford","Columbia","Berkeley","Mesa"])
+
+
+    petAllowed = random.choice(true_false)
+    randomEndDate = randomDate(randomStartDate, endDateEvent, random.random())
+    occupants = randint(2,7)
+
+    roommates = randint(1,3)
     
+    
+    housingPhoto = random.choice(housing_images)
+
+    text_file2.write(housing_lease_string.format(postID,streetName,buildingNumber,city,state,randomDateTime,description,amount,userID,auto_email,bedroom,bathroom,\
+                                       homeType,size,dateAvailable,furnished,hasParking,locality,petAllowed, randomEndDate,occupants, roommates,housingPhoto))
+    text_file2.write("\n")
+
+
+    user_string = '{{"userID": "{0}","firstName": "{1}", "lastName": "{2}", "phone": "{3}", "email": "{4}", "password": "{5}", "userPostings": {6}}}'
+
+    userFirstName = random.choice(["Jack","Peter","Anteater","David","Ray","Emily","Sophia","Jasmine"])
+    userLastName = random.choice(["Bill","Juan","Smith","Mata","Ronaldo","Muller","Messi","Suarez"])
+    userPhone = phn();
+    useremailID = generate_random_emails(10,7)[1];
+    userPassword = password_generator()
+    userPostings_list = userID_postID_list
+    
+    text_file.write(user_string.format(userID,userFirstName,userLastName,userPhone,useremailID,userPassword,userPostings_list))
+    text_file.write("\n")
 
     
     
